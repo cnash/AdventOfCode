a0a70fddff1801c49d755b61a252cc53ffbdd6b8
Chris Nash <cnash@kahua.com>
2023-12-22 11:46:12 -0500

    ...

--- 2023/08/Wasteland/Program.cs
+++ 2023/08/Wasteland/Program.cs
@@ -96,7 +96,7 @@ public class Wasteland
         Queue<string> locations = new Queue<string>(network.Keys.Where(k => k.EndsWith('A')));
         int locNum = 0;
         bool goLeft = true;
-        while (locations.Any()) //l.EndsWith('Z')))
+        while (locations.Any()) 
         {
             long steps = 0;
             locNum++;
@@ -117,60 +117,64 @@ public class Wasteland
                 }
                 steps++;
             }
-            Console.WriteLine($"Location #{locNum} - {steps}");
+            Console.WriteLine($"Location #{locNum} - Steps to Z: {steps}");
         }
     }
 
     public void Test3()
     {
         var (instructions, network) = readInputFile(inputFile);
+        var instructionsAsBools = instructions.Select(c => c == 'L').ToArray();
         Queue<string> locations = new Queue<string>(network.Keys.Where(k => k.EndsWith('A')));
-        int locNum = 0;
-        bool goLeft = true;
-        while (locations.Any())
+        List<List<string>> startingLocs = new List<List<string>>();
+        int numLocs = locations.Count;
+        for (int i=0;i<numLocs;i++)
         {
-            long steps = 0;
-            locNum++;
-            var firstLoc = locations.Dequeue();
-            var loc = firstLoc;
-            int idx = -1;
-            while (loc[2] != 'Z')
-            {
-                var entry = network[loc];
-                idx = (int)(steps % instructions.Length);
-                goLeft = instructions[idx] == 'L';
-
-                if (goLeft)
-                {
-                    loc = entry.Item1;
-                }
-                else
-                {
-                    loc = entry.Item2;
-                }
-                steps++;
-            }
-            var stepsToZ = steps;
-            while (!(idx == 0 && loc == firstLoc))
-            {
-                var entry = network[loc];
-                idx = (int)(steps % instructions.Length);
-                goLeft = instructions[idx] == 'L';
-
-                if (goLeft)
-                {
-                    loc = entry.Item1;
-                }
-                else
-                {
-                    loc = entry.Item2;
-                }
-                steps++;
-            }
-            var stepsToLoop = steps;
-
-            Console.WriteLine($"Location #{locNum} - {stepsToZ} - {stepsToLoop}");
+            startingLocs.Add(new List<string>());
         }
+        int locNum = 0;
+        long steps = 0;
+        while (!locations.All(l => l[2] == 'Z')) //l.EndsWith('Z')))
+        {
+            int idx = (int)(steps % instructions.Length);
+
+            if (idx == 0) 
+            {
+
+                for (int i=0;i<numLocs;i++)
+                {
+                    var x = locations.Dequeue();
+                    startingLocs[i].Add(x);
+                    locations.Enqueue(x);
+                }
+
+                var allVisited = startingLocs.First();
+                for (int i=1;i<startingLocs.Count;i++)
+                {
+                    allVisited = allVisited.Intersect(startingLocs[i]).ToList();
+                }
+                if (allVisited.Any())
+                {
+                    Console.WriteLine($"All paths have started at {string.Join(',', allVisited.ToArray())}");
+                }
+            }
+
+
+            for (int i = 0; i < numLocs; i++)
+            {
+                var entry = network[locations.Dequeue()];
+                if (instructionsAsBools[idx])
+                {
+                    locations.Enqueue(entry.Item1);
+                }
+                else
+                {
+                    locations.Enqueue(entry.Item2);
+                }
+            }
+            steps++;
+        }
+        Console.WriteLine($"Answer: {steps}");
     }
 
     private (string, Dictionary<string, Tuple<string, string>>) readInputFile(string inputFile)
