d9278b963bf948c7f33158c1478c783e3c1ce9a7
Chris Nash <cnash@kahua.com>
2023-12-22 14:28:30 -0500

    Day 9, Problem 2

--- 2023/09/Oasis/Program.cs
+++ 2023/09/Oasis/Program.cs
@@ -10,9 +10,11 @@ public class Oasis
     {
         Console.WriteLine("Sample Data:");
         (new Oasis()).Problem1(inputFileSample);
+        (new Oasis()).Problem2(inputFileSample);
 
         Console.WriteLine("Actual Data");
         (new Oasis()).Problem1(inputFile);
+        (new Oasis()).Problem2(inputFile);
     }
 
     public void Problem1(string filename)
@@ -51,6 +53,45 @@ public class Oasis
             return sequence.Last();
         } else {
             return sequence.Last() + nextNumberInSequence(newSequence);
+        }
+    }
+
+    public void Problem2(string filename)
+    {
+        try {
+        long answer = 0;
+        using (var rdr = File.OpenText(filename))
+        {
+            while (!rdr.EndOfStream)
+            {
+                var line = rdr.ReadLine();
+                var sequence = line
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(str => long.Parse(str))
+                    .ToList();
+                long next = previousNumberInSequence(sequence);
+                // Console.WriteLine($"Line: {line} - Next: {next}");
+                answer += next;
+            }
+        }
+        Console.WriteLine($"Problem 1: {answer}");
+        } catch (Exception exc) {
+            Console.WriteLine(exc.Message);
+        }
+    }
+
+    private long previousNumberInSequence(List<long> sequence)
+    {
+        List<long> newSequence = new List<long>();
+        for (var i=1; i<sequence.Count; i++)
+        {
+            newSequence.Add(sequence[i] - sequence[i-1]);
+        }
+        if (newSequence.All(x=>x==0))
+        {
+            return sequence.First();
+        } else {
+            return sequence.First() - previousNumberInSequence(newSequence);
         }
     }
 }
