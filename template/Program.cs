9f573a6818e176d2f8077925d555526b28a16672
Chris Nash <programmer@nashclan.net>
2024-12-21 09:15:24 -0500

    Added a template for use with `dotnet new`

---
+++ template/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace advent;
+
+public class Solver
+{
+    const string inputSample = @"..\..\..\input-sample";
+    const string inputActual = @"..\..\..\input-actual";
+    const int expectedSampleResult1 = 0;
+    const int expectedSampleResult2 = 0;
+
+    public static void Main()
+    {
+        long answer;
+
+        answer = new Solver().Problem1(inputSample);
+        Console.WriteLine($"Problem 1 Sample: Expected: {expectedSampleResult1}, Observed: {answer}");
+        if (answer != expectedSampleResult1) { return; }
+        answer = new Solver().Problem1(inputActual);
+        Console.WriteLine($"Problem 1 Actual: Observed: {answer}");
+
+        answer = new Solver().Problem2(inputSample);
+        Console.WriteLine($"Problem 2 Sample: Expected: {expectedSampleResult2}, Observed: {answer}");
+        if (answer != expectedSampleResult2) { return; }
+        answer = new Solver().Problem2(inputActual);
+        Console.WriteLine($"Problem 2 Actual: Observed: {answer}");
+    }
+    long Problem1(string inputFilePath)
+    {
+        return 0;
+    }
+
+    long Problem2(string inputFilePath)
+    {
+        return 0;
+    }
+
+
+}
